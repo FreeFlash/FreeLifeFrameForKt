a45ca70ce6b79e7aaef2a5f5da6b838b74803c56
田晓磊 <tianxiaolei@ppdai.com>
2018-01-17 15:42:06 +0800

    create IntentHelper

---
+++ app/src/main/java/com/flash/freelife/app/expand/Expand.kt
@@ -0,0 +1,10 @@
+package com.flash.freelife.app.expand
+
+/**
+ * Created by tianxiaolei on 2017/12/20.
+ */
+class Expand {
+    fun putString(key:String,value:String){
+
+    }
+}
---
+++ app/src/main/java/com/flash/freelife/app/navigation/IntentHelper.kt
@@ -0,0 +1,40 @@
+package com.flash.freelife.app.navigation
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+
+/**
+ * Created by tianxiaolei on 2017/12/20.
+ */
+class IntentBulider {
+    var intent: Intent? = null
+    fun createIntent(context: Context, clazz: Class<out Activity>): IntentBulider {
+        intent = Intent(context, clazz)
+        intent!!.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        return this
+    }
+
+    fun createIntent(context: Activity, clazz: Class<out Activity>): IntentBulider {
+        intent = Intent(context, clazz)
+        return this
+    }
+
+    fun putString(key:String,value:String){
+        intent?.putExtra(key,value)
+    }
+
+    fun putInt(key: String,value:Int){
+        intent?.putExtra(key,value)
+    }
+
+    fun addFlags(flags:Int){
+        intent?.addFlags(flags)
+    }
+
+    fun build(): Intent? {
+        return intent
+    }
+
+
+}
